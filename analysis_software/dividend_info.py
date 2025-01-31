38df70b382fb98a6eca137390d8be0bb1283563a
hanoweiss <hano7474@gmail.com>
2025-01-31 15:36:42 +0200

    push before mirsky is joining

---
+++ analysis_software/dividend_info.py
@@ -0,0 +1,9 @@
+import yfinance
+from datetime import *
+import pandas
+from yfinance import *
+
+
+symbol = 'AAPL'
+data = get_quote_data(symbol)
+
---
+++ analysis_software/options_strategy/wheel_strategy/wheel_screener.py
@@ -0,0 +1,9 @@
+from utils_functions import *
+
+
+
+data = get_data_from_api("AAPL")
+
+a=5
+
+
